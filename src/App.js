45e9b3d8f79cdd4f72e5e1532e1af789dd3c7079
jih22 <64046518+jih22@users.noreply.github.com>
2021-11-23 14:40:30 +0100

    Created dictionary file for search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by Argielyn Lapid</small>
+        </footer>
+      </div>
     </div>
   );
 }
